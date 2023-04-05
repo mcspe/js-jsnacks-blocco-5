13d4f5d89f3c5e22ca4d2b09a76c55e19571ffa3
mcspe <mcspezzaferro@gmail.com>
2023-04-05 02:54:16 +0200

    [add] grades display on hover

--- j-snack-2/script.js
+++ j-snack-2/script.js
@@ -60,12 +60,13 @@ const studentsAverageGrades = [];
 const studentsContainer = document.querySelector('.students-container');
 
 students.forEach(student => {
-  const {firstName, lastName, registrationNumber} = student;
+  const {firstName, lastName, registrationNumber, grades} = student;
   const averageGrade = (getAverageGrade(student.grades)).toFixed(2);
   const avatar = 'http://placebeard.it/480/notag';
   const obj = {
     name: `${lastName} ${firstName}`,
     registrationNumber,
+    grades,
     averageGrade,
     avatar
   }
@@ -81,7 +82,7 @@ studentsAverageGrades.sort((a, b) => {
 });
 
 
-studentsContainer.innerHTML = (studentsAverageGrades.map( (student) => {
+studentsContainer.innerHTML = (studentsAverageGrades.map( student => {
   const {name, registrationNumber, averageGrade, avatar} = student;
   const card = `
     <div class="card my-3 mx-auto">
@@ -89,12 +90,53 @@ studentsContainer.innerHTML = (studentsAverageGrades.map( (student) => {
       <div class="card-header">${name}</div>
       <div class="card-text">
         <p>Matricola: ${registrationNumber}</p>
-        <p>Media dei voti: ${averageGrade}</p>
+        <p class="grades-container">Media dei voti: ${averageGrade}</p>
       </div>
     </div>
   `
   return card;
 }).join(''));
+
+const gradesContainerCollection = document.querySelectorAll('.grades-container');
+const gradesContainer = Array.from(gradesContainerCollection);
+//console.log(gradesContainerCollection, gradesContainer);
+gradesContainer.forEach((container, i) => {
+  const arrowSpan = document.createElement('div');
+  arrowSpan.style.cursor = 'pointer';
+  arrowSpan.style.display = 'inline-block';
+  arrowSpan.style.position = 'relative';
+  arrowSpan.id = `arrow${i}`;
+  arrowSpan.innerHTML = '&#x25BE;';
+  container.append(arrowSpan);
+});
+
+studentsAverageGrades.forEach((student, i) => {
+  const arrow = document.getElementById(`arrow${i}`);
+  const arrowTooltip = document.createElement('div');
+  arrowTooltip.style = `display:none;`;
+  arrowTooltip.innerHTML = `Voti: ${ student.grades.join('<br>') }`;
+  arrow.append(arrowTooltip);
+  arrow.addEventListener('mouseenter',function(){
+    this.lastChild.style = `
+      visibility: visible;
+      width: 80px; 
+      background-color: #555;  
+      color: #fff;
+      text-align: right;
+      padding: 10px;
+      border-radius: 6px;
+      position: absolute;
+      z-index: 1;
+      bottom: 100%;
+      left: 50%;
+      margin-left: -60px;`;
+    //console.log(this.lastChild);
+  });
+  arrow.addEventListener('mouseleave',function(){
+    this.lastChild.style = `display:none;`;
+    //console.log(this.lastChild);
+  });
+});
 
 function getAverageGrade(grades){
   const average = grades.reduce((tot, grade) => tot + grade) / grades.length;
